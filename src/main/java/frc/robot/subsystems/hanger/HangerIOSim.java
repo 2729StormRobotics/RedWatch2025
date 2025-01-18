6dfd5f1276d3bbdfed0f349d58e18a8a88eae793
Isaiah151526 <151526@lrstudents.org>
2025-01-17 20:56:13 -0500

    Hanger simulation code
    Not complete

--- src/main/java/frc/robot/subsystems/hanger/HangerIOSim.java
+++ src/main/java/frc/robot/subsystems/hanger/HangerIOSim.java
@@ -8,8 +8,40 @@ import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 
-// public class HangerIOSim implements HangerIO{
+public class HangerIOSim implements HangerIO{
+
+  private static final DCMotor neoMotor = DCMotor.getNeo550(1);
+
+    private final DCMotorSim turnSim =
+        new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(turnMotorModel, 0.004, turnReduction),
+            turnMotorModel);
+
+ public void updateInputs(HangerIOInputs inputs) {
+
+    turnSim.update(LOOP_PERIOD_SECS);
+
+@Override
+
+public void robotInit(){
+
+    motor = new DCMotor( );// replace with specfic motor instance if needed
+
+    motorSim = new DCMotorSim(motor);
+    
+}
+
+@Override
+
+public void start(){
 
 
+ }
 
-// }
+@Override
+
+public void stop (){
+
+
+ }
+}
