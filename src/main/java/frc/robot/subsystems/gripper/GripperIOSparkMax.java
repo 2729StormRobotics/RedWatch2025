985e5e0db1b87d7723fa2004e51d3623d56024bf
ErickLin1 <172626@lrstudents.org>
2025-02-08 10:02:52 -0500

    changed to using sparklimitswitch

--- src/main/java/frc/robot/subsystems/gripper/GripperIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperIOSparkMax.java
@@ -1,41 +1,52 @@
 package frc.robot.subsystems.gripper;
 
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
+import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.LimitSwitchConfig.Type;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 public class GripperIOSparkMax implements GripperIO {
     private SparkMax m_gripperMotor;
     private SparkMaxConfig motorConfig;
 
-    private DigitalInput m_objectDetector;
-
-    private DigitalInput m_input;
+    private SparkLimitSwitch m_objectDetector;
 
     public GripperIOSparkMax() {
-        m_input = new DigitalInput(GripperConstants.beambreakPort);
         m_gripperMotor = new SparkMax(GripperConstants.gripperMotorPort, MotorType.kBrushless);
-        m_objectDetector = new DigitalInput(GripperConstants.beambreakPort);
+        m_objectDetector = m_gripperMotor.getForwardLimitSwitch();
 
         // Configure Motor
         motorConfig = new SparkMaxConfig();
         motorConfig.closedLoop.pid(GripperConstants.kPGripper, GripperConstants.kIGripper, GripperConstants.kDGripper);
+        motorConfig.limitSwitch.forwardLimitSwitchEnabled(false);
+        motorConfig.limitSwitch.forwardLimitSwitchType(Type.kNormallyOpen);
         motorConfig.idleMode(IdleMode.kCoast);
         motorConfig.smartCurrentLimit(40);
+
+        m_gripperMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         stop();
     }
 
+    /**
+     * Returns if an object is present in gripper
+     * @return boolean
+     */
     @Override
     public boolean isCoralPresent() {
-        return m_objectDetector.get();
+        return m_objectDetector.isPressed();
     }
 
+    /**
+     * Sets motor speed to 0
+     */
     @Override
     public void stopMotor() {
         m_gripperMotor.set(0);
