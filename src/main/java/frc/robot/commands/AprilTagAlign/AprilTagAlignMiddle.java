584ff77c17e92b67c72b5926098a90b4cdb08a63
cag5383 <124535031+cag5383@users.noreply.github.com>
2025-03-22 21:56:01 -0400

    Comments plus example/thoughts

--- src/main/java/frc/robot/commands/AprilTagAlign/AprilTagAlignMiddle.java
+++ src/main/java/frc/robot/commands/AprilTagAlign/AprilTagAlignMiddle.java
@@ -1,26 +1,18 @@
 package frc.robot.commands.AprilTagAlign;
 
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.math.MathUtil;
+//CAG:step one I'm blindly removing the yellow lines unless they throw an error just for readability.
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Constants;
-import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.subsystems.drive.DriveConstants;
 import frc.robot.subsystems.PhotonVision.VisionConstants;
 import frc.robot.subsystems.PhotonVision.VisionIO;
-import frc.robot.subsystems.PhotonVision.VisionIOPhoton.*;
-import frc.robot.subsystems.PhotonVision.VisionIO.*;
 
 import java.util.function.DoubleSupplier;
 
@@ -36,33 +28,38 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class AprilTagAlignMiddle extends Command implements VisionIO {
   private final Drive m_drivetrain;
-  private final Joystick m_translator;
   private final PIDController m_controller;
   private final PhotonCamera camera1;
-  private double m_turnError;
-  private double m_turnPower;
-  private DoubleSupplier xSupplier;
-  private DoubleSupplier ySupplier;
   private boolean seeingTargets = false;
   private Rotation2d rotation;
   private PIDController pidController;
   private double targetAngle;
 
+  // CAG: Looks like the x/y suppliers aren't currently required?
   public AprilTagAlignMiddle(Joystick joystick, Drive drivetrain,
       DoubleSupplier x_Supplier,
       DoubleSupplier y_Supplier) {
     m_drivetrain = drivetrain;
-    m_translator = joystick;
-    xSupplier = x_Supplier;
-    ySupplier = y_Supplier;
+    // CAG: I'm surprised there's only one controller and it's only for turning.
     m_controller = new PIDController(Constants.VisionConstants.kPTurn, Constants.VisionConstants.kITurn,
         Constants.VisionConstants.kDTurn);
     camera1 = new PhotonCamera(VisionConstants.outtake_Cam);
-  
+
+    // CAG: What do we need this for now?
     this.rotation = drivetrain.getRotation();
     targetAngle = 0;
     // Initialize PID controller (tune these values as needed)
+
+    // CAG: now we've got an m_controller and a this.pidController
+    // It's a little weird to reference the same kind of thing two different ways
+    // I'm more concerned about the name not being especially descriptive and that
+    // there's only one.
+    // At a glance, it seems like there's two different controllers for turning.
+    // Assuming that's the case, having two is super confusing/risky.
     this.pidController = new PIDController(0.006, 0.0, 0.0);
+    // CAG: The quick description I got was that all navigation is robot relative
+    // once you see a tag
+    // Assuming that's the case, a need for continuous input scares me.
     pidController.enableContinuousInput(-180, 180);
     pidController.setTolerance(1); // Allowable error in degrees
 
@@ -72,13 +69,14 @@ public class AprilTagAlignMiddle extends Command implements VisionIO {
   @Override
   public void initialize() {
     m_controller.reset();
-    m_turnError = 0;
-    m_turnPower = 0;
     SmartDashboard.putString("AprilTagAlign", "Initialized");
   }
 
   @Override
   public void execute() {
+    // CAG: the first get rotation seems unnecssary -
+    // it seems like you don't do anything with it and then immediately ask for a
+    // newer one.
     rotation = m_drivetrain.getRotation();
     // Read in relevant data from the Camera
     boolean targetVisible = false;
@@ -97,10 +95,24 @@ public class AprilTagAlignMiddle extends Command implements VisionIO {
           1.435, // Target Height
           Units.degreesToRadians(-30.0), // Camera Pitch
           Units.degreesToRadians(target.getPitch()));
-          
-    SmartDashboard.putNumber("id number", target.getFiducialId());
+
+      // CAG: Couple questions/thoughts here:
+      // 1) This looks like it's going to be pose/field-oriented rather than robot
+      // relative.
+      // I'm not sure it's a great idea to mix and match those if you use the
+      // targetRange/Yaw down the line.
+      // targetYaw depends on every component of your pose, targetRange depends on two
+      // The system gets complicated quickly if you're controlling measurements that
+      // depend on multiple controller outputs.
+      // 2) How does this behave if you can see multiple tags and the "best target"
+      // changes?
+      // 3) Do you have to do this every loop? Seems like you could set the target
+      // angle/fiducial ID
+      // In the constructor/init rather than doing it iteratively.
+      // Then just check that the fiducialID of the primary target hasn't changed?
+      SmartDashboard.putNumber("id number", target.getFiducialId());
       // FIND ANGLE FROM ID
-      switch(target.getFiducialId()) {
+      switch (target.getFiducialId()) {
         // BLUE SIDE TARGETS 18 is closet to DS
         case 22:
           targetAngle = -30;
@@ -156,42 +168,56 @@ public class AprilTagAlignMiddle extends Command implements VisionIO {
           // code block
           targetAngle = 0;
       }
+      // CAG: Why is the setpoint -targetAngle? Seems like this might be hiding a
+      // coordinate frame problem.
+      // CAG: Why do we need seeingTargets and targetVisible and results.hasTargets()?
       pidController.setSetpoint(-targetAngle);
       targetVisible = true;
     }
 
     // OFFSET PRESETS FOR DIFF OBJECTS (THIS IS LEFT)
+    // CAG: Curious why we're adding offsets if everything is robot relative.
+    // If everything is robot relative, you should be trying to get to the exact
+    // same pose relative to the target every time.
+    // I'm not sure you need to calculate that rather than just measuring it/polling
+    // the camera....
     double cameraToArmOffset = Units.inchesToMeters(6); // add when going right,
     // subtract when going left
-    double angleOffset = Units.radiansToDegrees(Math.atan((
-    cameraToArmOffset) / targetRange)); // use trig to calculate angle
-    
+    double angleOffset = Units.radiansToDegrees(Math.atan((cameraToArmOffset) / targetRange)); // use trig to calculate
+                                                                                               // angle
+//CAG: This is super confusing. The target yaw is a robot relative measurement, I'm not sure why we'd shift it this way.
+//If we shift something, I'd think it would be the setpoint
+//but even then we can just measure the robot-relative yaw when the position is correct and the offset should be included.
     targetYaw -= angleOffset; // subtract offset to go right add to go left
     // Override the driver's turn command with an automatic one that turns toward
     // the tag.
-    double lateralSpeed = -0.25 * targetYaw * Constants.VisionConstants.kPTurn * DriveConstants.kMaxSpeedMetersPerSecond;
-    double forward = forwardMultiplier * targetRange * Constants.VisionConstants.kPStrafe * DriveConstants.kMaxSpeedMetersPerSecond;
+
+    //CAG: These look like things that should be controllers.
+    //They're similar to controllers, but doing a lot of the math in roundabout/implicit ways instead of directly.
+    //I think there's a bunch of assumptions baked in here that may not always work out well.
+    //It's probably cleaner to just have three actual controllers, and to try to get them on independent variables.
+    // (X, Y, angle) in the same coordinate frame instead of different coordinate frames.
+    double lateralSpeed = -0.25 * targetYaw * Constants.VisionConstants.kPTurn
+        * DriveConstants.kMaxSpeedMetersPerSecond;
+    double forward = forwardMultiplier * targetRange * Constants.VisionConstants.kPStrafe
+        * DriveConstants.kMaxSpeedMetersPerSecond;
     // Put debug information to the dashboard
     SmartDashboard.putBoolean("Vision Target Visible", targetVisible);
     SmartDashboard.putNumber("Target Yaw", targetYaw);
     SmartDashboard.putNumber("Target range", targetRange);
     // Command drivetrain motors based on target speeds
-    double linearMagnitude = MathUtil.applyDeadband(
-        Math.hypot(
-            xSupplier.getAsDouble(), ySupplier.getAsDouble()),
-        0.02);
-    Rotation2d linearDirection = new Rotation2d(
-        xSupplier.getAsDouble(), ySupplier.getAsDouble());
-    double omega = MathUtil.applyDeadband(0.5, 0.02);
 
     // Get the current gyro angle
+    //CAG: you can just do this in one step back on line 80ish, right?
+    // currentAngle = m_drivetrain.getRotation().getDegrees() or something.
     double currentAngle = rotation.getDegrees();
 
     // Calculate the rotation speed using the PID controller
     double rotationSpeed = pidController.calculate(currentAngle);
-SmartDashboard.putNumber("currangle", currentAngle);
-SmartDashboard.putNumber("desiangle", targetAngle);
+    SmartDashboard.putNumber("currangle", currentAngle);
+    SmartDashboard.putNumber("desiangle", targetAngle);
     // Limit the speed to prevent over-rotation
+
     rotationSpeed = Math.max(-0.3, Math.min(0.3, rotationSpeed));
     SmartDashboard.putNumber("rotationspeed", rotationSpeed);
     // Convert to robot relative speeds & send command
@@ -205,19 +231,19 @@ SmartDashboard.putNumber("desiangle", targetAngle);
             new Rotation2d()));
 
     // if (targetYaw > 0) {
-    //   m_drivetrain.runVelocity(
-    //       ChassisSpeeds.fromRobotRelativeSpeeds(
-    //           m_drivetrain.getMaxLinearSpeedMetersPerSec() / 10,
-    //           0,
-    //           0,
-    //           new Rotation2d()));
+    // m_drivetrain.runVelocity(
+    // ChassisSpeeds.fromRobotRelativeSpeeds(
+    // m_drivetrain.getMaxLinearSpeedMetersPerSec() / 10,
+    // 0,
+    // 0,
+    // new Rotation2d()));
 
+    //CAG: This scares me - yaw depends on too many things to do this, I think.
     if (Math.abs(targetYaw) < yawThreshold) {
       lateralSpeed = 0;
     }
-    seeingTargets = !(targetRange>-1.8);
-    }
-
+    seeingTargets = !(targetRange > -1.8);
+  }
 
   @Override
   public void end(boolean interrupted) {
@@ -228,7 +254,11 @@ SmartDashboard.putNumber("desiangle", targetAngle);
   public boolean isFinished() {
     SmartDashboard.putBoolean("end bool", (m_controller.atSetpoint()) || !seeingTargets);
 
+    //CAG: I think this is probably a huge issue.
+    // This can end if only one of your three dimensions is at its setpoint, which I don't think is desired.
+    //I'm not sure I understand when seeingTargets is true/false or what it represents -
+    //There's a few too many things that are named similarly and not many comments/explanations.
     // End command when turn error is within tolerance
     return (m_controller.atSetpoint()) || !seeingTargets;
-  }}
-
+  }
+}
---
+++ src/main/java/frc/robot/commands/AprilTagAlign/CAGAprilTagAlignMiddle.java
@@ -0,0 +1,129 @@
+package frc.robot.commands.AprilTagAlign;
+
+//CAG:step one I'm blindly removing the yellow lines unless they throw an error just for readability.
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.PhotonVision.VisionConstants;
+import frc.robot.subsystems.PhotonVision.VisionIO;
+
+import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
+/**
+ * Command to align the robot with the nearest AprilTag using PhotonVision while
+ * still allowing translational driving.
+ */
+
+public class CAGAprilTagAlignMiddle extends Command implements VisionIO {
+  private final Drive m_drivetrain;
+  private final PhotonCamera camera1;
+  private PIDController pid_turn;
+  private PIDController pid_x;
+  private PIDController pid_y;
+  private double targetX_m; // CAG: assuming the photon vision output is meters.
+  private double targetY_m; // CAG: assuming the photon vision output is meters.
+  private double targetYaw_deg; // CAG: assuming the photon vision output is degrees.
+
+  // CAG: You could pass in the target, robot-relative X, Y, angle as arguemnts
+  // Rather than having separate Left, Right, Middle commands. Separate commands
+  // is clearer though.
+  public CAGAprilTagAlignMiddle(Drive drivetrain) {
+    m_drivetrain = drivetrain;
+    // CAG: I'm surprised there's only one controller and it's only for turning.
+    camera1 = new PhotonCamera(VisionConstants.outtake_Cam);
+
+    // Initialize PID controller (tune these values as needed)
+    // CAG: No idea what any of the constants/tolerances should be - that's a
+    // testing problem.
+    this.pid_turn = new PIDController(0, 0, 0);
+    pid_turn.setTolerance(0);
+    pid_turn.setSetpoint(targetYaw_deg); //CAG: have to actually/intelligently set/determine the targets.
+
+    this.pid_x = new PIDController(0, 0, 0);
+    pid_x.setTolerance(0);
+    pid_x.setSetpoint(targetX_m);
+
+    this.pid_y = new PIDController(0, 0, 0);
+    pid_y.setTolerance(0);
+    pid_y.setSetpoint(targetY_m);
+
+
+    addRequirements(m_drivetrain);
+  }
+
+  @Override
+  public void initialize() {
+  }
+
+  @Override
+  public void execute() {
+
+    PhotonPipelineResult results = camera1.getLatestResult();
+
+    if (results.hasTargets()) {
+      // At least one AprilTag was seen by the camera
+
+      // CAG: you can do some kind of conditional check on whether
+      // you see one of the reef tags here and break out early/set an end condition
+
+      // CAG: You could also presumably lock a particular fiducial tag based on:
+      // 1) what you see on init
+      // 2) Your closest target based on pose
+      // 3) A specific target based on a parent command
+      // If you have a particular target you could presumably loop through all the
+      // targets
+      // and look for the one in question to avoid issues where you snap between
+      // "best" targets.
+      // I have no idea how necessary any of that is.
+
+    }
+
+    //CAG: I'm assuming we only get here if we have data from a relevant tag.
+
+    // CAG: I'm not 100% that this works.
+    // But I think this gives you X,Y,Z of the tag relative to the camera.
+    // I'm not sure whether the target or the camera is the reference point, but I'm
+    // not sure it matters.
+    // If you can use these plus yaw, you have an indepdent X, Y, and angle that are
+    // in the same coordinate frame.
+    // Because all of these measurements are relative to the tag, they should be the
+    // same at every reef point.
+    // YOu should just be able to measure what the X, Y, and yaw are when you're in
+    // a good spot.
+    // Those should be the setpoints for every reef position.
+    PhotonTrackedTarget target = results.getBestTarget();
+    double currYaw = target.getYaw();
+    Transform3d translation = target.getBestCameraToTarget();
+
+    double currX = translation.getX();
+    double currY = translation.getY();
+
+    double rotationSpeed = pid_turn.calculate(currYaw);
+    double xSpeed = pid_x.calculate(currX);
+    double ySpeed = pid_y.calculate(currY);
+
+    //CAG: I'm not sure this is necessary, but you can definitely do it.
+    //Could also just tune PID output, but it can be safe to clamp the output.
+
+    rotationSpeed = Math.max(-0.3, Math.min(0.3, rotationSpeed));
+    m_drivetrain.runVelocity(
+        ChassisSpeeds.fromRobotRelativeSpeeds(ySpeed,xSpeed,rotationSpeed,m_drivetrain.getRotation()));
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    SmartDashboard.putString("AprilTagAlign", interrupted ? "Interrupted" : "Completed");
+  }
+
+  @Override
+  public boolean isFinished() {
+
+    //CAG: Check that all the controllers are happy before killing it.
+    return (pid_turn.atSetpoint() && pid_x.atSetpoint() && pid_y.atSetpoint());
+  }
+}
