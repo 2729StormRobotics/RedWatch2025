cbc021b1b93cfc95af403cfe10c4ec6c6514ffa5
KrithikAlluri <118485611+KrithikAlluri@users.noreply.github.com>
2025-03-24 09:32:26 -0400

    recomment and recode gripper subsystem
    added hold command

--- src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperConstants.java
@@ -1,17 +1,18 @@
 package frc.robot.subsystems.gripper;
 
-
-
+/**
+ * Constants for the Gripper subsystem.
+ */
 public class GripperConstants {
-    public static final int gripperMotorPort = 13;
+    public static final int gripperMotorPort = 13; // Port for the gripper motor
 
-    public static final int beambreakPort = 2; // Have to update after testing
+    public static final int beambreakPort = 2; // Port for the beambreak sensor (to be updated after testing)
 
-    public static final double motorSpeedInGripper = -0.15; // Have to update after testing
+    public static final double motorSpeedInGripper = -0.15; // Motor speed for intaking (to be updated after testing)
 
-    public static final double motorSpeedOutGripper = -1;
+    public static final double motorSpeedOutGripper = -1; // Motor speed for outtaking
 
-    public static final double kPGripper = 0;
-    public static final double kIGripper = 0;
-    public static final double kDGripper = 0;
+    public static final double kPGripper = 0.003; // Proportional gain for gripper position control
+    public static final double kIGripper = 0; // Integral gain for gripper position control
+    public static final double kDGripper = 0; // Derivative gain for gripper position control
 }
--- src/main/java/frc/robot/subsystems/gripper/GripperIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/gripper/GripperIOSparkMax.java
@@ -12,78 +12,99 @@ import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.LimitSwitchConfig.Type;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+/**
+ * Implementation of the GripperIO interface using a Spark Max motor controller.
+ */
 public class GripperIOSparkMax implements GripperIO {
-    private SparkMax m_gripperMotor;
-    private SparkMaxConfig motorConfig;
+    private SparkMax m_gripperMotor; // Spark Max motor controller for the gripper
+    private SparkMaxConfig motorConfig; // Configuration for the Spark Max
 
-    //private SparkLimitSwitch m_objectDetector;
-
-
+    /**
+     * Constructor for GripperIOSparkMax.
+     */
     public GripperIOSparkMax() {
-        m_gripperMotor = new SparkMax(GripperConstants.gripperMotorPort, MotorType.kBrushless);
-        //m_objectDetector = m_gripperMotor.getForwardLimitSwitch();
+        m_gripperMotor = new SparkMax(GripperConstants.gripperMotorPort, MotorType.kBrushless); // Initialize Spark Max
 
         // Configure Motor
         motorConfig = new SparkMaxConfig();
-        motorConfig.closedLoop.pid(GripperConstants.kPGripper, GripperConstants.kIGripper, GripperConstants.kDGripper);
-        motorConfig.limitSwitch.forwardLimitSwitchEnabled(false);
-        motorConfig.limitSwitch.forwardLimitSwitchType(Type.kNormallyOpen);
-        motorConfig.idleMode(IdleMode.kBrake);
-        motorConfig.smartCurrentLimit(40);
+        motorConfig.closedLoop.pid(GripperConstants.kPGripper, GripperConstants.kIGripper, GripperConstants.kDGripper); // Configure PID
+        motorConfig.limitSwitch.forwardLimitSwitchEnabled(false); // Disable forward limit switch
+        motorConfig.limitSwitch.forwardLimitSwitchType(Type.kNormallyOpen); // Set limit switch type
+        motorConfig.idleMode(IdleMode.kBrake); // Set idle mode to brake
+        motorConfig.smartCurrentLimit(40); // Set current limit
 
-        m_gripperMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        m_gripperMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters); // Apply configuration
 
-        stop();
+        stop(); // Stop the motor initially
     }
 
     /**
-     * Returns if an object is present in gripper
+     * Returns if an object is present in gripper.
      * @return boolean
      */
     @Override
     public boolean isCoralPresent() {
-        return m_gripperMotor.getForwardLimitSwitch().isPressed();
+        return m_gripperMotor.getForwardLimitSwitch().isPressed(); // Check forward limit switch
     }
 
     /**
-     * Sets motor speed to 0
+     * Sets motor speed to 0.
      */
     @Override
     public void stop() {
-        m_gripperMotor.set(0);
+        m_gripperMotor.set(0); // Stop the motor
     }
 
     /**
-     * Sets motor speed to inwards
+     * Sets motor speed to inwards.
      */
     @Override
     public void setMotorIn() {
-        m_gripperMotor.set(GripperConstants.motorSpeedInGripper);
+        m_gripperMotor.set(GripperConstants.motorSpeedInGripper); // Set motor to intake speed
     }
 
- /**
+    /**
      * Sets motor speed to inwards
      */
     @Override
     public void reverse() {
-        m_gripperMotor.set(0.6);
+        m_gripperMotor.set(0.6); // Reverse the motor
     }
 
     /**
-     * Sets motor speed to outwards
+     * Sets motor speed to outwards.
      */
     @Override
     public void setMotorOut() {
-        m_gripperMotor.set(GripperConstants.motorSpeedOutGripper);
+        m_gripperMotor.set(GripperConstants.motorSpeedOutGripper); // Set motor to outtake speed
     }
 
+    /**
+     * Updates the GripperIOInputs with current sensor data.
+     * @param inputs GripperIOInputs to update.
+     */
     @Override
     public void updateInputs(GripperIOInputs inputs) {
-        
-        inputs.gripperAppliedVolts = (m_gripperMotor.getBusVoltage() * m_gripperMotor.getAppliedOutput());
-        inputs.gripperPositionDegrees = 0;
-        inputs.gripperVelocityRadPerSec = m_gripperMotor.getEncoder().getVelocity();
+        inputs.gripperAppliedVolts = (m_gripperMotor.getBusVoltage() * m_gripperMotor.getAppliedOutput()); // Update applied voltage
+        inputs.gripperPositionDegrees = 0; // Update position (currently 0)
+        inputs.gripperVelocityRadPerSec = m_gripperMotor.getEncoder().getVelocity(); // Update velocity
     }
-    
-    
+
+    /**
+     * Sets the motor speed directly.
+     * @param speed The speed to set the motor to.
+     */
+    @Override
+    public void setMotorSpeed(double speed) {
+        m_gripperMotor.set(speed); // Set motor speed
+    }
+
+    /**
+     * Gets the motor encoder position.
+     * @return The encoder position.
+     */
+    @Override
+    public double getMotorPos() {
+        return m_gripperMotor.getEncoder().getPosition(); // Get encoder position
+    }
 }
